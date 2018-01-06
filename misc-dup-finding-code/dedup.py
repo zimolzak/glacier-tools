e99edd7f2f5199d0c26b1477aa8af49db029d82f
Andrew Zimolzak <andyzimolzak@gmail.com>
2018-01-06 16:07:58 -0500

    Put globals in all caps.

--- misc-dup-finding-code/dedup.py
+++ misc-dup-finding-code/dedup.py
@@ -6,11 +6,11 @@
 from subprocess import getoutput
 import os
 
-command_b = "find /Users/ajz/Music/iTunes/iTunes\ Media/Music"
-#command_b = "find /Users/ajz/Music/iTunes/iTunes\ Media/Music/Weezer"
-command_a = "find /Users/ajz/powerbook/Users/ajz/Music/iTunes/iTunes\ Music"
+COMMAND_B = "find /Users/ajz/Music/iTunes/iTunes\ Media/Music"
+#COMMAND_B = "find /Users/ajz/Music/iTunes/iTunes\ Media/Music/Weezer"
+COMMAND_A = "find /Users/ajz/powerbook/Users/ajz/Music/iTunes/iTunes\ Music"
 
-debug = False
+DEBUG = False
 
 ########
 
@@ -48,52 +48,52 @@ def reference_lists(find_lines):
 
 ########
 
-find_lines_a = getoutput(command_a).splitlines()
-[path_list_a, size_list_a] = reference_lists(find_lines_a)
+FIND_LINES_A = getoutput(COMMAND_A).splitlines()
+[PATH_LIST_A, SIZE_LIST_A] = reference_lists(FIND_LINES_A)
 
-######## Begin processing of B. Depends on these global vars: size_list_a, path_list_a.
+######## Begin processing of B. Depends on these global vars: SIZE_LIST_A, PATH_LIST_A.
 
-find_lines_b = getoutput(command_b).splitlines()
-ub = []
-size_list_b = []
-path_list_b = []
-sha_list_a = []
-sha_list_b = []
-path_matching_a = []
+FIND_LINES_B = getoutput(COMMAND_B).splitlines()
+UNIQ_B = []
+SIZE_LIST_B = []
+PATH_LIST_B = []
+SHA_LIST_A = []
+SHA_LIST_B = []
+PATH_MATCHING_A = []
 
-for line in find_lines_b:
+for line in FIND_LINES_B:
     if 'mp3' in line or 'm4a' in line:
         path_b = line
         size_b = os.path.getsize(path_b)
         sha_a = sha_b = ' '*40
-        if size_b in size_list_a:
-            path_a = path_list_a[size_list_a.index(size_b)]
-            path_matching_a.append(path_a)
+        if size_b in SIZE_LIST_A:
+            path_a = PATH_LIST_A[SIZE_LIST_A.index(size_b)]
+            PATH_MATCHING_A.append(path_a)
             sha_a = sha_path(path_a)
             sha_b = sha_path(path_b)
             if sha_a == sha_b:
-                ub.append(False)
+                UNIQ_B.append(False)
             else:
-                ub.append(True)
+                UNIQ_B.append(True)
         else: # size of file B is unique
-            ub.append(True)
-            path_matching_a.append(None)
-        size_list_b.append(size_b)
-        sha_list_a.append(sha_a)
-        sha_list_b.append(sha_b)
-        path_list_b.append(path_b)
+            UNIQ_B.append(True)
+            PATH_MATCHING_A.append(None)
+        SIZE_LIST_B.append(size_b)
+        SHA_LIST_A.append(sha_a)
+        SHA_LIST_B.append(sha_b)
+        PATH_LIST_B.append(path_b)
 
 ######## print
 
-for i, p in enumerate(path_list_b):
-    if sha_list_a[i] == ' '*40: # Element of B is unique in size
-        assert ub[i]
+for i, p in enumerate(PATH_LIST_B):
+    if SHA_LIST_A[i] == ' '*40: # Element of B is unique in size
+        assert UNIQ_B[i]
         print(p)
-    elif sha_list_a[i] != ' '*40 and ub[i]: # same exact size different hash
+    elif SHA_LIST_A[i] != ' '*40 and UNIQ_B[i]: # same exact size different hash
         print(p)
-        print('    #### ' + path_matching_a[i])
+        print('    #### ' + PATH_MATCHING_A[i])
     else:
-        if debug:
-            print([size_list_b[i], sha_list_a[i], sha_list_b[i], ub[i], path2file(p)])
+        if DEBUG:
+            print([SIZE_LIST_B[i], SHA_LIST_A[i], SHA_LIST_B[i], UNIQ_B[i], path2file(p)])
         else:
             pass
